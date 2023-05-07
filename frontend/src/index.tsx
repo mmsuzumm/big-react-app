b5b1f9014cf99f344874eb754256dfb3bb0deb13
Matv <97804343+mmsuzumm@users.noreply.github.com>
2023-05-08 01:44:13 +0300

    Revert "small start settings update"
    This reverts commit 5e32561acf783680de8d4d469299f2fdfdc75f49.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './components/screen/home/Home';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
 
