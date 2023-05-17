2a343de27dd2f1c5089f66a0ff557ab9fb1729d5
Matv <97804343+mmsuzumm@users.noreply.github.com>
2023-05-17 19:26:42 +0300

    better python code

--- backend/main.py
+++ backend/main.py
@@ -1,44 +1,15 @@
-from fastapi import FastAPI, Request
-from pydantic import BaseModel
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from routers import users
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    # Разрешаем запросы со всех источников. Можно указать список допустимых источников.
     allow_origins=["http://localhost:3000"],
-    # Разрешаем передавать куки и заголовки авторизации вместе с запросом.
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
-    # Разрешаем все заголовки в запросе.
     allow_headers=["*"],
 )
 
-
-class UserLogin(BaseModel):
-    email: str
-    password: str
-
-
-class NewUserRegistration(BaseModel):
-    username: str
-    email: str
-    password: str
-    rePassword: str
-    role: str
-
-
-@app.post("/api/user/registration")
-async def user_register(request: Request, data: NewUserRegistration):
-    new_user_data = await request.json()
-    print(new_user_data)
-    return {"status": "ok", "message": "Регистрация прошла успешно"}
-
-
-@app.post("/api/user/login")
-async def user_login(request: Request, data: UserLogin):
-    user_data = await request.json()
-    print(user_data)
-    return {"status": "ok", "message": "Регистрация прошла успешно"}
+app.include_router(users.router)
